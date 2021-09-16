55269707a25c3952c0c24216965b426eb9745778
Rayssa Besse <53128035+rayssabesse@users.noreply.github.com>
2021-09-15 21:17:04 -0300

    Update senai-rental-02-DML.sql

--- senai_sprint2_backend-main/exercicios/01_rental/Senai.Rental.WebApi/Scripts/senai-rental-02-DML.sql
+++ senai_sprint2_backend-main/exercicios/01_rental/Senai.Rental.WebApi/Scripts/senai-rental-02-DML.sql
@@ -10,10 +10,10 @@ INSERT INTO MODELO (nomeModelo, idMarca)
 VALUES('M3', 1), ('320I', 1), ('R8', 2)
 
 INSERT INTO CLIENTE (nomeCliente, sobrenomeCliente, cpfCliente)
-VALUES('YURI', 'Chibo', 333), ('HENRIQUE', 'Ohnesorge', 666)
+VALUES('RAYSSA', 'Besse', 333), ('ARMANDO', 'Besse', 666)
 
 INSERT INTO ALUGUEL (idCliente, dataRetirada, dataDevolucao)
-VALUES(1, '08/08/2021', '18/08/2021'), (2, '01/08/2021', '11/08/2021')
+VALUES(1, '02/09/2021', '12/09/2021'), (2, '03/09/2021', '13/09/2021')
 
 INSERT INTO VEICULO (idEmpresa, idModelo, idAluguel, placaVeiculo)
 VALUES(1, 2, 1, '012938'), (2, 1, 2, '47U898')
